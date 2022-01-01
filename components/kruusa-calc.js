eb396c242055dd2783e1a5c055b06fb01ff2bd3b
wargunnerguy <reimovellemaa@gmail.com>
2022-01-01 21:43:48 +0100

    esialgne tabel

---
+++ components/kruusa-calc.js
@@ -0,0 +1,117 @@
+Vue.component('kruusa-calc', {
+	data() {
+		return {
+			fraktsioonTitle: 'Fraktsioonmees'
+		}
+	},
+	template: `
+<section class="wrapper">
+    <!-- row-t title -->
+    <div class="row-t title">
+      <ul>
+        <li>Fraktsioon</li>
+        <li>Hind/t</li>
+        <li>Km 20%</li>
+        <li>Hind+km/t</li>
+      </ul>
+    </div>
+    <!-- row-t 3 -->
+    <article class="row-t nfl">
+      <ul>
+        <li><a href="#">NHL</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+  
+    <!-- row-t 5 -->
+    <article class="row-t mlb">
+      <ul>
+        <li><a href="#">MLB</a></li>
+        <li>$5</li>
+        <li>48</li>
+        <li>120</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 6 -->
+    <article class="row-t nhl">
+      <ul>
+        <li><a href="#">NHL</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 7 -->
+    <article class="row-t nhl">
+      <ul>
+        <li><a href="#">NHL</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 8 -->
+    <article class="row-t pga">
+      <ul>
+        <li><a href="#">PGA</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 9 -->
+    <article class="row-t pga">
+      <ul>
+        <li><a href="#">PGA</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 10 -->
+    <article class="row-t pga">
+      <ul>
+        <li><a href="#">PGA</a></li>
+        <li>$50</li>
+        <li>12</li>
+        <li>48</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+    <!-- row-t 11 -->
+    <article class="row-t mlb">
+      <ul>
+        <li><a href="#">MLB</a></li>
+        <li>$10</li>
+        <li>1</li>
+        <li>10</li>
+      </ul>
+      <ul class="more-content">
+        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+      </ul>
+    </article>
+  </section>
+`
+});
