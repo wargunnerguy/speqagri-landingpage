7d613041b7b4cef3c0f7209eb96f7013b327da29
wargunnerguy <reimovellemaa@gmail.com>
2022-01-02 11:16:17 +0100

    initial v-for calc

--- components/kruusa-calc.js
+++ components/kruusa-calc.js
@@ -1,7 +1,35 @@
 Vue.component('kruusa-calc', {
 	data() {
 		return {
-			fraktsioonTitle: 'Fraktsioonmees'
+			products: [
+				{
+					id: 1,
+					name: "Lubjakivikillustik",
+					type: 'mlb',
+					fraction: '0-10 mm',
+					pricePerTon: 3.5,
+					tax: 0.70,
+					desc: 'This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!',
+				},
+				{
+					id: 2,
+					name: "Lubjakivikillustik",
+					type: 'mlb',
+					fraction: '0-16 mm',
+					pricePerTon: 5.00,
+					tax: 0.70,
+					desc: '',
+				},
+				{
+					id: 3,
+					type: 'nhl',
+					name: "Täiteliiv",
+					fraction: 'Täiteliiv',
+					pricePerTon: 6.00,
+					tax: 1.20,
+					desc: '',
+				}
+			],
 		}
 	},
 	template: `
@@ -16,19 +44,19 @@ Vue.component('kruusa-calc', {
       </ul>
     </div>
     <!-- row-t 3 -->
-    <article class="row-t nfl">
+    <article v-for="product in products" :key="product.id" class="row-t" :class="product.type">
       <ul>
-        <li><a href="#">NHL</a></li>
-        <li>$50</li>
-        <li>12</li>
-        <li>48</li>
+        <li><a href="#">{{ product.fraction }}</a></li>
+        <li>{{ product.pricePerTon }} €/t</li>
+        <li>{{ product.tax }} €</li>
+        <li>{{ product.pricePerTon + product.tax }} €/t</li>
       </ul>
       <ul class="more-content">
-        <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
+        <li>{{ product.desc }}</li>
       </ul>
     </article>
   
-    <!-- row-t 5 -->
+<!--    &lt;!&ndash; row-t 5 &ndash;&gt;
     <article class="row-t mlb">
       <ul>
         <li><a href="#">MLB</a></li>
@@ -40,7 +68,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 6 -->
+    &lt;!&ndash; row-t 6 &ndash;&gt;
     <article class="row-t nhl">
       <ul>
         <li><a href="#">NHL</a></li>
@@ -52,7 +80,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 7 -->
+    &lt;!&ndash; row-t 7 &ndash;&gt;
     <article class="row-t nhl">
       <ul>
         <li><a href="#">NHL</a></li>
@@ -64,7 +92,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 8 -->
+    &lt;!&ndash; row-t 8 &ndash;&gt;
     <article class="row-t pga">
       <ul>
         <li><a href="#">PGA</a></li>
@@ -76,7 +104,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 9 -->
+    &lt;!&ndash; row-t 9 &ndash;&gt;
     <article class="row-t pga">
       <ul>
         <li><a href="#">PGA</a></li>
@@ -88,7 +116,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 10 -->
+    &lt;!&ndash; row-t 10 &ndash;&gt;
     <article class="row-t pga">
       <ul>
         <li><a href="#">PGA</a></li>
@@ -100,7 +128,7 @@ Vue.component('kruusa-calc', {
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
     </article>
-    <!-- row-t 11 -->
+    &lt;!&ndash; row-t 11 &ndash;&gt;
     <article class="row-t mlb">
       <ul>
         <li><a href="#">MLB</a></li>
@@ -111,7 +139,7 @@ Vue.component('kruusa-calc', {
       <ul class="more-content">
         <li>This 1665-player contest boasts a $300,000.00 prize pool and pays out the top 300 finishing positions. First place wins $100,000.00. Good luck!</li>
       </ul>
-    </article>
+    </article>-->
   </section>
 `
 });
