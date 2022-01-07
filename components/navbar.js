c05970e1e43c941272b6cec0831dc108be638ea5
wargunnerguy <reimovellemaa@gmail.com>
2022-01-07 16:35:44 +0100

    disable calc on nav

--- components/navbar.js
+++ components/navbar.js
@@ -108,7 +108,7 @@ Vue.component('navbar', {
                             <li>
                                 <a v-bind:title="$t('message.contact')" href="index.html#contact">{{ $t("message.contact") }}</a>
                             </li>
-                            <li><a v-bind:title="$t('message.calculator')" href="kalkulaator.html">{{ $t("message.calculator") }}</a></li>
+<!--                            <li><a v-bind:title="$t('message.calculator')" href="kalkulaator.html">{{ $t("message.calculator") }}</a></li>-->
                             <li>
                                 <a @click=changeToEstonian() href="#" onclick="event.preventDefault();"><img src="assets/img/flags/et.png"/></a>
                             </li>
